15b0fcfbaa1147d1aea598fcfb948954b2c96a06
Ketan Kharche <ketan3393@gmail.com>
2017-07-27 12:16:02 -0700

    feat: Creat C++ file with skeleton code

---
+++ time_in_words.cpp
@@ -0,0 +1,16 @@
+nclude <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    int h;
+    cin >> h;
+    int m;
+    cin >> m;
+    return 0;
+}
+
