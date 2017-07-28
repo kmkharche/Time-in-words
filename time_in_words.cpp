ddd489df814622b047c382fa62f8d653af593b9e
Ketan Kharche <ketan3393@gmail.com>
2017-07-28 02:16:14 -0700

    feat: Function to convert from numbers to words

--- time_in_words.cpp
+++ time_in_words.cpp
@@ -1,10 +1,79 @@
-nclude <cmath>
+#include <cmath>
 #include <cstdio>
 #include <vector>
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
+void converter(int t)
+{
+    switch (t){
+        case 1:
+            cout << "one ";
+            break;
+        case 2:
+            cout << "two ";
+            break;
+        case 3:
+            cout << "three ";
+            break;
+        case 4:
+            cout << "four ";
+            break;
+        case 5:
+            cout << "five ";
+            break;
+        case 6:
+            cout << "six ";
+            break;
+        case 7:
+            cout << "seven ";
+            break;
+        case 8:
+            cout << "eight ";
+            break;
+        case 9:
+            cout << "nine ";
+            break;
+        case 10:
+            cout << "ten ";
+            break;
+        case 11:
+            cout << "eleven ";
+            break;
+        case 12:
+            cout << "twelve ";
+            break;
+        case 13:
+            cout << "thirteen ";
+            break;
+        case 14:
+            cout << "fourteen ";
+            break;
+        case 15:
+            cout << "quarter ";
+            break;
+        case 16:
+            cout << "sixteen ";
+            break;
+        case 17:
+            cout << "seventeen ";
+            break;
+        case 18:
+            cout << "eighteen ";
+            break;
+        case 19:
+            cout << "nineteen ";
+            break;
+        case 20:
+            cout << "twenty ";
+            break;
+        case 30:
+            cout << "half ";
+            break;
+    }
+    return;
+}
 
 int main(){
     int h;
